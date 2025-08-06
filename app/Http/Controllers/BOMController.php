6f98ed9ea62e372d97baa23a32843dff907a65e8
FadelAnfasha <delanfashap25@gmail.com>
2025-08-06 11:07:08 +0700

    HEHEHE

--- app/Http/Controllers/BOMController.php
+++ app/Http/Controllers/BOMController.php
@@ -253,13 +253,16 @@ class BOMController extends Controller
 
         BOM_Report::truncate();
 
+
+
         foreach ($groups as $group) {
             $main = $group->first(); // FG
             $main->load(['processCost', 'materialInfo']);
+            dd($group->disc->materialInfo->quantity);
 
-            $disc_price = ceil((($group->disc->materialInfo->standardPrice ?? 0) * $group->disc->materialInfo->quantity) * 100) / 100;
-            $rim_price = ceil((($group->rim->materialInfo->standardPrice ?? 0) * $group->rim->materialInfo->quantity) * 100) / 100;
-            $sidering_price = ceil((($group->sidering->materialInfo->standardPrice ?? 0) * $group->sidering->materialInfo->quantity) * 100) / 100;
+            $disc_price = $group->disc->materialInfo->standardPrice ?? 0;
+            $rim_price = $group->rim->materialInfo->standardPrice ?? 0;
+            $sidering_price = $group->sidering->materialInfo->standardPrice ?? 0;
 
             $pr_cedW_price = ceil(((($main->processCost->max_of_ced ?? null) * 5) / 7) * 100) / 100;
             $pr_cedSR_price = ceil(((($main->processCost->max_of_ced ?? null) * 2) / 7) * 100) / 100;
